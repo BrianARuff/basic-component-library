91f6871a0013b253a8870022a12ddebc4debda33
Brian Ruff <brff19@gmail.com>
2024-10-06 05:46:58 -0400

    update version

--- build/components/Accordion/Accordion.d.ts
+++ build/components/Accordion/Accordion.d.ts
@@ -1,9 +1,11 @@
 import React, { ReactNode } from "react";
-export declare const Accordion: (props: {
-    isDisabled: boolean;
-    renderContent: ({ isOpen, setIsOpen, }: {
-        isOpen: boolean;
-        isDisabled: boolean;
-        setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    }) => ReactNode;
-}) => React.JSX.Element;
+export type AccordionRenderContentProps = {
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export type AccordionProps = {
+    isDisabled?: boolean;
+    renderBodyContent?: ({ isOpen, setIsOpen, }: AccordionRenderContentProps) => ReactNode;
+    renderHeaderContent?: ({ isOpen, setIsOpen, }: AccordionRenderContentProps) => ReactNode;
+};
+export declare const Accordion: (props: AccordionProps) => React.JSX.Element;
--- build/index.js
+++ build/index.js
@@ -3,18 +3,17 @@
 var React = require('react');
 
 var Accordion = function (props) {
-    var _a;
-    var _b = React.useState(false), isOpen = _b[0], setIsOpen = _b[1];
+    var _a, _b;
+    var _c = React.useState(false), isOpen = _c[0], setIsOpen = _c[1];
     var id = React.useId();
-    return (React.createElement("div", null,
-        React.createElement("button", { onClick: function () { return setIsOpen(function (prev) { return !prev; }); }, type: "button", "aria-expanded": isOpen, id: "".concat(id, "Button") }, "Click To Toggle Accordion Content"),
-        isOpen && !props.isDisabled
-            ? (_a = props === null || props === void 0 ? void 0 : props.renderContent) === null || _a === void 0 ? void 0 : _a.call(props, {
+    return (React.createElement("div", { "data-testid": "accordion-container" },
+        React.createElement("button", { "data-testid": "accordion-button", onClick: function () { return setIsOpen(function (prev) { return !prev; }); }, type: "button", "aria-expanded": isOpen, "aria-controls": "".concat(id, "Content"), "aria-label": "Toggle Accordion", id: "".concat(id, "Button") }, (_a = props === null || props === void 0 ? void 0 : props.renderHeaderContent) === null || _a === void 0 ? void 0 : _a.call(props, { isOpen: isOpen, setIsOpen: setIsOpen })),
+        React.createElement("div", { "aria-labelledby": "".concat(id, "Button"), id: "".concat(id, "Content"), role: "region", hidden: !isOpen }, isOpen && !props.isDisabled
+            ? (_b = props === null || props === void 0 ? void 0 : props.renderBodyContent) === null || _b === void 0 ? void 0 : _b.call(props, {
                 isOpen: isOpen,
                 setIsOpen: setIsOpen,
-                isDisabled: props === null || props === void 0 ? void 0 : props.isDisabled,
             })
-            : null));
+            : null)));
 };
 
 exports.Accordion = Accordion;
